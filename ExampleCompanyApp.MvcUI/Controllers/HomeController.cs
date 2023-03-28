8ab98cce81ecdc18976cc1fc59cec3fed9e2f6f5
Alpaslan Akgün <alpaslan.akgun@outlook.com>
2023-03-28 16:05:58 +0300

    Add project files.

---
+++ ExampleCompanyApp.MvcUI/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using ExampleCompanyApp.MvcUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ExampleCompanyApp.MvcUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
